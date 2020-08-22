5b0d0364e948a7ebff0e497c558341083c91c292
yuliethc <yuliethdotcom@gmail.com>
2020-08-22 12:04:31 -0500

    Narcissistic Number excercise and New HTML and CSS

--- JS/spiral.js
+++ JS/spiral.js
@@ -42,6 +42,8 @@
  * Excercise finished by @madacol
  * 
  */
+
+
 const X = 0
 const Y = 1
 
@@ -56,48 +58,46 @@ const RIGHT = [1 ,0]
 const UP    = [0 ,-1]
 const LEFT  = [-1,0]
 
-const SEQUENCE = [
+const ROTATION_SEQUENCE = [
     DOWN,
     RIGHT,
     UP,
     LEFT
 ]
 
-function vectorSum(a,b) {
-    return a.map((val, i) => (val + b[i]))
+function getNextPos(pos, dir) {
+    // Perform a vector sum
+    return [
+        (pos[X] + dir[X]),
+        (pos[Y] + dir[Y])
+    ]
 }
-function changeDir(dir) {
-    const i = SEQUENCE.findIndex(seqDir=>((seqDir[X]===dir[X])&&(seqDir[Y]===dir[Y])))
-    const next_i = (i+1) % 4
-    return SEQUENCE[next_i]
+function changeDir(dir, offsetIndex=1) {
+    const i = ROTATION_SEQUENCE.findIndex(seqDir=>seqDir===dir)
+    const next_i = (i+offsetIndex) % 4
+    return ROTATION_SEQUENCE[next_i]
 }
-function isPosValid(pos, limits, dir) {
-
+// Mutable
+function reduceLimits(limits, dir) {
+    const oppositeDir = changeDir(dir, 2)
+    const amountToReduce = dir.find(value=>value!==0)
+    limits[oppositeDir]+=amountToReduce
+}
+function isPosInsideLimits(pos, limits, dir) {
+    // find axis
     const axis = dir.findIndex(coord=>coord!==0)
     const scalar = pos[axis]
-    const [start, end] = limits[axis]
-    const isStartValid = (start <= scalar)
-    const isEndValid = (scalar <= end)
-    const isValid = isStartValid && isEndValid
-    {
-        // Update limits
-        if (!isStartValid) {
-            limits[axis][START]++
-            return false
-        }
-        if (!isEndValid) {
-            limits[axis][END]--
-            return false
-        }
-    }
-    return true
+    const limit = limits[dir]
+    const isDirIncreasing = dir[axis] === 1
+    return isDirIncreasing ? (scalar <= limit) : (limit <= scalar)
 }
 function getNextPosition(pos, dir, limits) {
-    let nextPosCandidate = vectorSum(pos, dir);
-    if (!isPosValid(nextPosCandidate, limits, dir)) {
+    let nextPosCandidate = getNextPos(pos, dir);
+    if (!isPosInsideLimits(nextPosCandidate, limits, dir)) {
         dir = changeDir(dir);
-        nextPosCandidate = vectorSum(pos, dir);
-        if (!isPosValid(nextPosCandidate, limits, dir)) return false // End of spiral
+        reduceLimits(limits, dir);
+        nextPosCandidate = getNextPos(pos, dir);
+        if (!isPosInsideLimits(nextPosCandidate, limits, dir)) return false // End of spiral
     }
     pos = nextPosCandidate;
 
@@ -105,11 +105,9 @@ function getNextPosition(pos, dir, limits) {
 }
 // Mutable
 function writeNumToMatrix(num, pos, matrix) {
+    console.log(pos)
     return matrix[pos[Y]][pos[X]] = num;
 }
-
-// const cmd = getNextPosition([0, 4], [0, 1], [[0, 3], [0, 4]])
-// console.log(cmd)
 
 /**
  * Calculate the size needed for a square matrix
@@ -125,22 +123,27 @@ function spiralfill(data) {
     let pos = [0,0];
     let dir = DOWN;
     let limits = {
-        [X]: [1, matrixRowSize-1],
-        [Y]: [0, matrixRowSize-1]
+        [DOWN]: matrixRowSize-1,
+        [RIGHT]: matrixRowSize-1,
+        [UP]: 0,
+        [LEFT]: 0
     }
     const lastIndex = matrixRowSize**2;
     for (let i = 0; i < lastIndex; i++) {
         const num = data[i] || 0;
         writeNumToMatrix(num, pos, matrix);
         const result = getNextPosition(pos, dir, limits)
+        if (!result) {
+            console.error("Finished")
+            return {matrix};
+        }
         pos = result.pos;
         dir = result.dir;
     }
 
-    return {matrix};
 }
 
 /* Implement and run test cases for your function */
-const resultados = spiralfill([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]);
+const resultados = spiralfill([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]);
 
 console.log(resultados);
