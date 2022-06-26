8c0bab6f919f034e99c8587d6044b0df8e004acc
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-06-26 19:01:11 +0200

    practice

--- JS/disemvowellTroll.js
+++ JS/disemvowellTroll.js
@@ -9,3 +9,16 @@ For example, the string "This website is for losers LOL!" would become "Ths wbst
 Note: for this kata y isn't considered a vowel.
 
  */
+
+function disemvowel(str) {
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  var strArray = str.split("");
+  var newStr = "";
+  for (var i = 0; i < strArray.length; i++) {
+    if (vowels.indexOf(strArray[i]) === -1) {
+      //if the letter is not a vowel
+      newStr += strArray[i];
+    } //end if
+  } //end for/
+  return newStr;
+} //end function  //
---
+++ JS/sum2.js
@@ -0,0 +1,67 @@
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ */
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+//  var twoSum = function(a, b) { 
+//     c=[]
+//     process1: for (let i = 0; i < a.length; i++) {
+//                 for (let index = i+1; index < a.length; index++) {
+//                     if ((a[i]+a[index]) == b){
+//                      c.push(i,index)
+//                      break process1
+//                     }   
+//                  }
+//              }
+//     return c
+// };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(a, b) {  
+    for (let i = 0; i < a.length; i++) {
+     for (let index = i+1; index < a.length; index++) {
+         if ((a[i]+a[index]) == b){
+          return [i,index];
+         }   
+      }
+  }
+ };
